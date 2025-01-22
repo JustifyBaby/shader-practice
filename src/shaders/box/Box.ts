a82512b4466824f39c41ebe1b428fb7f255c4a4c
JustifyBaby <quronoa1232@gmail.com>
2025-01-23 06:54:46 +0900

    整理

--- src/shaders/box/Box.ts
+++ src/shaders/box/Box.ts
@@ -4,15 +4,6 @@ import * as THREE from "three";
 
 export const boxGeometry = new THREE.BoxGeometry(5, 5, 5);
 
-const count = boxGeometry.attributes["position"].count;
-const rands = new Float32Array(count);
-
-for (let i = 0; i < count; i++) {
-  rands[i] = Math.random();
-}
-
-boxGeometry.setAttribute("aRands", new THREE.BufferAttribute(rands, 1));
-
 export const boxMaterial = new THREE.RawShaderMaterial({
   vertexShader,
   fragmentShader,
--- src/shaders/plane/Plane.ts
+++ src/shaders/plane/Plane.ts
@@ -3,16 +3,6 @@ import vertexShader from "./vertexShader.glsl";
 import * as THREE from "three";
 
 export const planeGeometry = new THREE.PlaneGeometry(1, 1, 32, 32);
-const count = planeGeometry.attributes["position"].count;
-const rands = new Float32Array(count);
-
-for (let i = 0; i < count; i++) {
-  rands[i] = Math.random() * 2;
-}
-const varying = new THREE.BufferAttribute(rands, 1);
-
-planeGeometry.setAttribute("aRands", varying);
-console.log(varying);
 
 export const planeMaterial = new THREE.RawShaderMaterial({
   vertexShader,
